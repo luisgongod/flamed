e0c47acf379b31f36b6030682dfe006f62c95553
luisgongod <luis.gongod@gmail.com>
2020-06-04 19:19:10 +0200

    ✨ wii-nunchaku mapped.

--- node-sender/src/stick.cpp
+++ node-sender/src/stick.cpp
@@ -5,62 +5,124 @@
 #include <OSCMessage.h>
 #include <OSCBundle.h>
 #include <OSCData.h>
+#include <NintendoExtensionCtrl.h>
 #include "credentials.h"
 
+#define LOOPDELAY 50
+
+WiFiUDP Udp;
+const IPAddress outIp(192,168,2,73); // remote IP (not needed for receive)
+const unsigned int outPort = 9000;		// remote port (not needed for receive)
+const unsigned int localPort = 8000;
+OSCErrorCode error;
+
+Nunchuk nchuk;
+
 //To work with: IMU_Stick.noisette
-/
 float pitch = 0.0F;
 float roll  = 0.0F;
 float yaw   = 0.0F;
 
-WiFiUDP Udp;
-OSCErrorCode error;
-
-float pitch_mx = 0;
-float roll_mx = 0;
-float yaw_mx = 0;
-
-float pitch_mn = 0;
-float roll_mn = 0;
-float yaw_mn = 0;
-
 void setup() {
-  M5.begin();
-  M5.IMU.Init();
-  M5.Lcd.setRotation(3);
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setTextSize(1);
-  M5.Lcd.setCursor(40, 0);
-  M5.Lcd.println("IMU TEST");
-  M5.Lcd.setCursor(0, 10);
-  M5.Lcd.setCursor(0, 50);
-  M5.Lcd.println("  Pitch   Roll    Yaw");
-  
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
-
-  while (WiFi.status() != WL_CONNECTED){
+	Serial.begin(115200);
+	delay(100);
+	Serial.println("On a Stick!");
+	
+	nchuk.begin();
+	while (!nchuk.connect()) {
+		Serial.println("Nunchuk not detected!");
+		delay(500);
+	}
+	
+	WiFi.begin(ssid, pass);	
+	while (WiFi.status() != WL_CONNECTED){
 		delay(500);
 		Serial.print(".");
 	}
-  Udp.begin(localPort);
 
+	Udp.begin(localPort);
+	
+	M5.begin();
+	M5.IMU.Init();
+	M5.Lcd.setRotation(3);
+	M5.Lcd.fillScreen(BLACK);
+	M5.Lcd.setTextSize(1);
+	M5.Lcd.setCursor(40, 0);
+	M5.Lcd.println("IMU TEST");
+	M5.Lcd.setCursor(0, 10);
+	M5.Lcd.setCursor(0, 50);
+	M5.Lcd.println("  Pitch   Roll    Yaw");	
 }
 
 float temp = 0;
 void loop() {  
-  M5.IMU.getAhrsData(&pitch,&roll,&yaw);  
-  M5.Lcd.setCursor(0, 60);
-  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", pitch, roll, yaw);
-    delay(100);
+//IMU
+	M5.IMU.getAhrsData(&pitch,&roll,&yaw);  
+	M5.Lcd.setCursor(0, 60);
+	M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", pitch, roll, yaw);    
+
+//OSC_IMU
+	OSCBundle bndl;
+	bndl.add("/imu/pitch").add(pitch);
+	bndl.add("/imu/roll").add(roll);
+	bndl.add("/imu/yaw").add(yaw);
+	Udp.beginPacket(outIp, outPort);
+	bndl.send(Udp); 
+	Udp.endPacket(); 
+	bndl.empty();  
 
 
-//OSC
-  OSCBundle bndl;
-  bndl.add("/imu").add(pitch).add(roll).add(yaw);
-  Udp.beginPacket(outIp, outPort);
-  bndl.send(Udp); // send the bytes to the SLIP stream
-  Udp.endPacket(); // mark the end of the OSC Packet
-  bndl.empty(); // empty the bundle to free room for a new one
-  
+//Wii Nchuck
+	boolean success = nchuk.update();  // Get new data from the controller
+
+//.. to debug if there are connection issues.
+	// if (success == true) {  // We've got data!
+	// 	nchuk.printDebug();  // Print all of the values!
+	// }
+	// else {  // Data is bad :(
+	// 	Serial.println("Controller Disconnected!");
+	// 	delay(1000);
+	// 	nchuk.connect();		
+	// }
+
+	// bndl.add("/nkacc/x").add(nchuk.accelX());
+	// bndl.add("/nkacc/y").add(nchuk.accelY());
+	// bndl.add("/nkacc/z").add(nchuk.accelZ());
+	bndl.add("/nkimu/roll").add(nchuk.rollAngle());
+	bndl.add("/nkimu/pitch").add(nchuk.pitchAngle());
+	Udp.beginPacket(outIp, outPort);
+	bndl.send(Udp); 
+	Udp.endPacket(); 
+	bndl.empty();  
+
+	bndl.add("/nkjoy/x").add(nchuk.joyX());
+	bndl.add("/nkjoy/y").add(nchuk.joyY());
+	Udp.beginPacket(outIp, outPort);
+	bndl.send(Udp); 
+	Udp.endPacket(); 
+	bndl.empty();  	
+
+	boolean zButton = nchuk.buttonZ();
+	boolean cButton = nchuk.buttonC();
+	
+	if (zButton == true) {
+		bndl.add("/nkbtt/z").add(1);	
+	}
+	else{
+		bndl.add("/nkbtt/z").add(0);	
+	}
+
+	if (cButton == true) {
+		bndl.add("/nkbtt/c").add(1);	
+	}
+	else{
+		bndl.add("/nkbtt/c").add(0);	
+	}
+	Udp.beginPacket(outIp, outPort);
+	bndl.send(Udp); 
+	Udp.endPacket(); 
+	bndl.empty();  
+
+
+	delay(LOOPDELAY);
 }
